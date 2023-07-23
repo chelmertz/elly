c7296e6982d6f615cf33b7ef35eb8673686c6cff
Calle Helmertz <helmertz@gmail.com>
2023-07-23 22:50:12 +0200

    i3blocks: Open elly with left click
    1. It's a bit spammy to open a lot of browser tabs with a default
       action
    2. There was no quick way to open elly. Now there is.

--- contrib/elly_i3blocks.sh
+++ contrib/elly_i3blocks.sh
@@ -6,14 +6,22 @@ set -euo pipefail
 
 text_or_icon="$1"
 
-prs=$(curl -q 'http://localhost:9876/api/v0/prs?minPoints=0')
+elly_url=http://localhost:9876
+prs=$(curl -q "$elly_url/api/v0/prs?minPoints=0")
 count=$(jq 'length' <(echo "$prs"))
 if [ "$count" -gt 0 ]; then
 	echo "$text_or_icon $count"
 	echo "$text_or_icon $count"
 	echo "#00ff00"
 
+ 	# left click - open elly in browser
 	if [ "$BLOCK_BUTTON" -eq 1 ]; then
+		xdg-open "$elly_url" &
+		wmctrl -a chrome
+	fi
+
+	# right click - open all PRs in browser, one by one
+	if [ "$BLOCK_BUTTON" -eq 3 ]; then
 		for u in $(jq -r '.[].Url' <(echo "$prs")); do
 			xdg-open "$u" &
 			wmctrl -a chrome
