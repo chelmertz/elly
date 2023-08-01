eee1d229fb9f62608dbe355859af2f6f3cd0ef4d
Calle Helmertz <helmertz@gmail.com>
2023-08-01 22:02:00 +0200

    i3: Open elly with left click even if 0 prs

--- contrib/elly_i3blocks.sh
+++ contrib/elly_i3blocks.sh
@@ -8,17 +8,18 @@ text_or_icon="$1"
 
 elly_url=http://localhost:9876
 prs=$(curl -q "$elly_url/api/v0/prs?minPoints=0")
+
+# left click - open elly in browser
+if [ "$BLOCK_BUTTON" -eq 1 ]; then
+	xdg-open "$elly_url" &
+	wmctrl -a chrome
+fi
+
 count=$(jq 'length' <(echo "$prs"))
 if [ "$count" -gt 0 ]; then
 	echo "$text_or_icon $count"
 	echo "$text_or_icon $count"
 	echo "#00ff00"
-
- 	# left click - open elly in browser
-	if [ "$BLOCK_BUTTON" -eq 1 ]; then
-		xdg-open "$elly_url" &
-		wmctrl -a chrome
-	fi
 
 	# right click - open all PRs in browser, one by one
 	if [ "$BLOCK_BUTTON" -eq 3 ]; then
