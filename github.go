b30869b107edabd5b6996c7ff75ddb227d95de50
Calle Helmertz <helmertz@gmail.com>
2023-08-09 00:14:43 +0200

    Extract points.go and github.go
    Everything-in-one-file had a good run. It got annoying to deal with. I'm
    keeping everything in the main package for now, but knowing that
    "graphql is modified in github.go" etc. will help me find things
    quicker.

---
+++ github.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"golang.org/x/exp/slog"
+)
+
+var errClient = errors.New("github returned client error")
+var errGithubServer = errors.New("github returned server error")
+var errCouldNotStorePrs = errors.New("could not store prs")
+
+func possiblyRefreshPrs(token, username string, storage *storage) (bool, error) {
+	// querying github once a minute should be fine,
+	// especially as long as we do the passive, loopy thing more seldom
+	if time.Since(storage.Prs().LastFetched) < time.Duration(59)*time.Second {
+		return false, nil
+	}
+	prs, err := queryGithub(token, username)
+	if err != nil {
+		return false, fmt.Errorf("could not query github: %w", err)
+	}
+
+	if err := storage.StoreRepoPrs(prs); err != nil {
+		return false, fmt.Errorf("%w: %w", errCouldNotStorePrs, err)
+	}
+	return true, nil
+}
+
+type querySearchPrsInvolvingMeGraphQl struct {
+	Data struct {
+		Search struct {
+			Edges []struct {
+				Node struct {
+					Url            string
+					Title          string
+					IsDraft        bool
+					ReviewRequests struct {
+						Nodes []struct {
+							RequestedReviewer struct {
+								Login string
+							}
+						}
+					}
+					ReviewDecision string
+					UpdatedAt      string
+					Author         struct {
+						Login string
+					}
+
+					Repository struct {
+						Url   string
+						Name  string
+						Owner struct {
+							Login string
+						}
+					}
+
+					Additions int
+					Deletions int
+					Comments  struct {
+						Edges []struct {
+							Node struct {
+								UpdatedAt string
+								Author    struct {
+									Login string
+								}
+								Url  string
+								Body string
+							}
+						}
+					}
+					Commits struct {
+						Nodes []struct {
+							Commit struct {
+								Author struct {
+									Date  string
+									Email string
+									Name  string
+								}
+							}
+						}
+					}
+					ReviewThreads struct {
+						Edges []struct {
+							Node struct {
+								IsResolved  bool
+								IsOutdated  bool
+								IsCollapsed bool
+								Comments    struct {
+									Nodes []struct {
+										Author struct {
+											Login string
+										}
+										Body string
+										Url  string
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func queryGithub(token string, username string) ([]ViewPr, error) {
+	payload := struct {
+		Query string `json:"query"`
+	}{
+		Query: querySearchPrsInvolvingUser(username),
+	}
+	jsonBytes, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal json: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	httpClient := &http.Client{}
+
+	request, err := http.NewRequestWithContext(ctx, "POST", "https://api.github.com/graphql", bytes.NewReader(jsonBytes))
+	request.Header.Add("Authorization", "bearer "+token)
+	if err != nil {
+		return nil, fmt.Errorf("could not construct github request: %w", err)
+	}
+
+	logger.Info("querying github api")
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("could not request github: %w", err)
+	}
+	defer response.Body.Close()
+
+	respBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read github response: %w", err)
+	}
+
+	if response.StatusCode >= 400 {
+		logger.Warn("response", slog.Int("response_code", response.StatusCode), slog.String("body", string(respBody)))
+		if response.StatusCode < 500 {
+			return nil, errClient
+		}
+		return nil, errGithubServer
+	}
+
+	typedResponse := querySearchPrsInvolvingMeGraphQl{}
+	err = json.Unmarshal(respBody, &typedResponse)
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal github response: %w", err)
+	}
+
+	viewPrs := make([]ViewPr, 0)
+
+	for _, prEdge := range typedResponse.Data.Search.Edges {
+		pr := prEdge.Node
+
+		updatedAt, err := time.Parse(time.RFC3339, pr.UpdatedAt)
+		if err != nil {
+			// not really a fatal error, just log it
+			logger.Warn("could not parse time", slog.String("updatedAt", pr.UpdatedAt), slog.String("pr_url", pr.Url))
+			updatedAt = time.Time{}
+		}
+
+		lastPrCommenter := ""
+		for _, c := range pr.Comments.Edges {
+			lastPrCommenter = c.Node.Author.Login
+		}
+
+		unresolvedReviewThreadLastCommenters := make([]string, 0)
+		for _, t := range pr.ReviewThreads.Edges {
+			if t.Node.IsCollapsed || t.Node.IsOutdated || t.Node.IsResolved {
+				continue
+			}
+
+			lastCommenting := t.Node.Comments.Nodes[len(t.Node.Comments.Nodes)-1].Author.Login
+			unresolvedReviewThreadLastCommenters = append(unresolvedReviewThreadLastCommenters, lastCommenting)
+		}
+
+		reviewUsers := make([]string, 0)
+		for _, u := range pr.ReviewRequests.Nodes {
+			reviewUsers = append(reviewUsers, u.RequestedReviewer.Login)
+		}
+
+		viewPr := ViewPr{
+			ReviewStatus:                         pr.ReviewDecision,
+			Url:                                  pr.Url,
+			Title:                                pr.Title,
+			Author:                               pr.Author.Login,
+			RepoName:                             pr.Repository.Name,
+			RepoOwner:                            pr.Repository.Owner.Login,
+			RepoUrl:                              pr.Repository.Url,
+			IsDraft:                              pr.IsDraft,
+			LastUpdated:                          updatedAt,
+			LastPrCommenter:                      lastPrCommenter,
+			UnresolvedReviewThreadLastCommenters: unresolvedReviewThreadLastCommenters,
+			Additions:                            pr.Additions,
+			Deletions:                            pr.Deletions,
+			ReviewRequestedFromUsers:             reviewUsers,
+		}
+		logger.Debug("fetched a pr", slog.Any("pr", viewPr))
+		viewPrs = append(viewPrs, viewPr)
+	}
+
+	return viewPrs, nil
+}
+
+func querySearchPrsInvolvingUser(username string) string {
+	query := `query {
+  search(type: ISSUE, query: "state:open involves:%s type:pr archived:false", first: 100) {
+    edges {
+      node {
+        ... on PullRequest {
+          title
+          url
+          isDraft
+          reviewRequests(first: 100) {
+            nodes {
+              requestedReviewer {
+                ... on User {
+                  login
+                }
+              }
+            }
+          }
+          repository {
+            url
+            name
+            owner {
+              login
+            }
+          }
+          reviewDecision
+          updatedAt
+          author {
+            login
+          }
+          additions
+          deletions
+          comments(last: 5) {
+            edges {
+              node {
+                updatedAt
+                author {
+                  login
+                }
+                url
+                body
+              }
+            }
+          }
+          commits(last: 1) {
+            nodes {
+              commit {
+                author {
+                  date
+                  email
+                  name
+                }
+                status {
+                  contexts {
+                    state
+                    context
+                    description
+                    createdAt
+                    targetUrl
+                  }
+                }
+              }
+            }
+          }
+          reviewThreads(first: 20) {
+            edges {
+              node {
+                isResolved
+                isOutdated
+                isCollapsed
+                comments(first: 100) {
+                  nodes {
+                    author {
+                      login
+                    }
+                    body
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+	return fmt.Sprintf(query, username)
+}
---
+++ points.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+type Points struct {
+	Total   int
+	Reasons []string
+}
+
+func (p *Points) Add(points int, reason string) {
+	p.Total += points
+	reasonWithPrefix := fmt.Sprintf("+%d: %s", points, reason)
+	p.Reasons = append(p.Reasons, reasonWithPrefix)
+}
+
+func (p *Points) Remove(points int, reason string) {
+	p.Total -= points
+	reasonWithPrefix := fmt.Sprintf("-%d: %s", points, reason)
+	p.Reasons = append(p.Reasons, reasonWithPrefix)
+}
+
+// standardPrPoints() awards points to PRs based on a set of rules.
+// These rules should be revisited often, and the points should be tweaked.
+func standardPrPoints(pr ViewPr, username string) *Points {
+	points := &Points{}
+	points.Reasons = make([]string, 0)
+
+	unrespondedThreads := unrespondedCommentThreads(pr, username)
+
+	if pr.Author == username {
+		// our pr
+		if pr.ReviewStatus == "APPROVED" {
+			points.Add(100, "Own PR is approved, should be a simple merge")
+		}
+
+		if pr.ReviewStatus == "CHANGES_REQUESTED" {
+			points.Add(50, "Someone wants you to change something")
+		}
+
+		if pr.LastPrCommenter != "" && pr.LastPrCommenter != username {
+			// someone might have asked us something
+			points.Add(10, fmt.Sprintf("Someone else commented last (%s)", pr.LastPrCommenter))
+		}
+
+		if unrespondedThreads > 0 {
+			points.Add(80, fmt.Sprintf("Someone asked us something (%d comments)", unrespondedThreads))
+			// we already need to go over this, don't scale the points
+			// by amount of threads though, it might go overboard
+		}
+
+		if pr.IsDraft {
+			points.Remove(10, "PR is my draft")
+		}
+
+		if len(pr.ReviewRequestedFromUsers) == 0 {
+			// points represents "actions to take", and if there are no
+			// reviewers assigned to your pr, there's a chance no-one will look
+			// at it
+			points.Add(10, "You should add reviewers")
+		}
+	} else {
+		// someone else's pr, or our but the username is not set
+		if pr.ReviewStatus == "APPROVED" {
+			points.Remove(100, "PR is someone else's and is approved")
+		}
+
+		if pr.ReviewStatus == "CHANGES_REQUESTED" {
+			// you might want to wait with this, it seems like the PR author has
+			// some work to do already
+			points.Remove(10, "Changes are already requested")
+		}
+
+		if pr.IsDraft {
+			points.Remove(100, "PR is someone else's draft")
+		}
+
+		// reward short prs
+		diff := int(math.Abs(float64(pr.Additions)) + math.Abs(float64(pr.Deletions)))
+		switch {
+		case diff < 50:
+			points.Add(50, fmt.Sprintf("PR is small, %d loc changed is <50", diff))
+		case diff < 150:
+			points.Add(30, fmt.Sprintf("PR is smallish, %d loc changed is <150", diff))
+		case diff <= 300:
+			points.Add(20, fmt.Sprintf("PR is bigger, %d loc changed is <=300", diff))
+		case diff > 300:
+			points.Add(10, fmt.Sprintf("PR is bigish, %d loc changed is >300", diff))
+		}
+
+		// TODO find our own comment threads here, and see if they are
+		// responded to
+	}
+
+	sort.Slice(points.Reasons, func(i, j int) bool {
+		// render all + points first, then - points
+		return points.Reasons[i] < points.Reasons[j]
+	})
+
+	return points
+}
+
+// unrespondedCommentThreads() searches through PR review comments and returns
+// amount of comments that the given github user has not responded to.
+func unrespondedCommentThreads(pr ViewPr, myGithubUsername string) int {
+	comments := 0
+	if myGithubUsername == "" {
+		return comments
+	}
+
+	for _, c := range pr.UnresolvedReviewThreadLastCommenters {
+		if c != myGithubUsername {
+			comments++
+		}
+	}
+
+	return comments
+}
