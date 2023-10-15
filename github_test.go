1cd1c528f6c4dc31186c6196252069612e407465
Calle Helmertz <helmertz@gmail.com>
2023-10-15 12:00:47 +0200

    fuzz multiple files

--- github_test.go
+++ github_test.go
@@ -1,12 +1,15 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func Test_WhenReviewThreadIsEmpty_WillNotRequireAction(t *testing.T) {
 	constructedButEmpty := actionableThreads(prSearchResultGraphQl{ReviewThreads: struct {
 		Edges []struct{ Node prReviewThreadGraphQl }
 	}{
-		Edges: []struct{ Node prReviewThreadGraphQl }{struct{ Node prReviewThreadGraphQl }{Node: prReviewThreadGraphQl{Comments: struct {
+		Edges: []struct{ Node prReviewThreadGraphQl }{{Node: prReviewThreadGraphQl{Comments: struct {
 			Nodes []prReviewThreadCommentGraphQl
 		}{}}}},
 	},
@@ -23,23 +26,109 @@ func Test_WhenReviewThreadIsEmpty_WillNotRequireAction(t *testing.T) {
 	}
 }
 
-//func Fuzz_WhenReviewThreadsExist_WillCountUnresponded(f *testing.F) {
-//	myUsername := "currentUser"
-//	unrespondedCount := actionableThreads(prSearchResultGraphQl{ReviewThreads: struct {
-//		Edges []struct{ Node prReviewThreadGraphQl }
-//	}{
-//		Edges: []struct{ Node prReviewThreadGraphQl }{struct{ Node prReviewThreadGraphQl }{Node: prReviewThreadGraphQl{Comments: struct {
-//			Nodes []prReviewThreadCommentGraphQl
-//		}{
-//			[]prReviewThreadCommentGraphQl{prReviewThreadCommentGraphQl{Author: struct{ Login string }{Login: "currentUser"}}},
-//		}}}},
-//	},
-//	}, "currentUser")
-//
-//	// unresponded == I can take action on it
-//
-//	// others pr + myUsername is not part of the thread == 0
-//	// if count > 0: myUsername is not last comment
-//	// our pr + myUsername is not part of the thread > 0
-//	// only myUsername commented == 0
-//}
+func commentBy(username string) prReviewThreadCommentGraphQl {
+	return prReviewThreadCommentGraphQl{Author: struct{ Login string }{Login: username}}
+}
+
+func Fuzz_WhenReviewThreadsExist_WillCountUnresponded(f *testing.F) {
+	myUsername := "itsMe"
+	othersUsername := "otherUser"
+
+	f.Add(true, true, false, uint(3), int64(0))
+	f.Add(false, true, false, uint(3), int64(0))
+
+	f.Fuzz(func(t *testing.T, myPr bool, firstCommentIsMine bool, lastCommentIsMine bool, numberOfComments uint, randomSeed int64) {
+		// TODO we're looking at a single review thread per test, there should be a test that looks at multiple threads too (but that test could skip the calculation of points, and only care about the sum of the counts)
+		allComments := make([]prReviewThreadCommentGraphQl, 0)
+		if numberOfComments > 0 {
+			if firstCommentIsMine {
+				allComments = append(allComments, commentBy(myUsername))
+			} else {
+				allComments = append(allComments, commentBy(othersUsername))
+			}
+			random := rand.New(rand.NewSource(randomSeed))
+			// "-2" because we compensate for firstCommentIsMine and lastCommentIsMine
+			for i := 0; uint(i) < numberOfComments-2; i++ {
+				// we're hoping to cover most real cases, there are monologues, back and forths, unresponded comments, etc.
+				if random.Int()%2 == 0 {
+					allComments = append(allComments, commentBy(myUsername))
+				} else {
+					allComments = append(allComments, commentBy(othersUsername))
+				}
+			}
+			if numberOfComments > 1 {
+				if lastCommentIsMine {
+					allComments = append(allComments, commentBy(myUsername))
+				} else {
+					allComments = append(allComments, commentBy(othersUsername))
+				}
+			}
+		}
+
+		var prAuthor string
+		if myPr {
+			prAuthor = myUsername
+		} else {
+			prAuthor = othersUsername
+		}
+		threads := prSearchResultGraphQl{
+			Author: struct{ Login string }{Login: prAuthor},
+			ReviewThreads: struct {
+				Edges []struct{ Node prReviewThreadGraphQl }
+			}{
+				Edges: []struct{ Node prReviewThreadGraphQl }{{Node: prReviewThreadGraphQl{Comments: struct {
+					Nodes []prReviewThreadCommentGraphQl
+				}{
+					allComments,
+				}}}},
+			},
+		}
+
+		actionableThreads := actionableThreads(threads, myUsername)
+
+		if numberOfComments == 0 {
+			if actionableThreads != 0 {
+				t.Errorf("got %d unresponded threads, expected 0, since there are no comments", actionableThreads)
+			}
+			// no need to check anything further, skip the rest to avoid "if
+			// len(comments)>0" checks all over
+			t.Skip("numberOfComments == 0")
+		}
+
+		if myPr && lastCommentIsMine {
+			if actionableThreads != 0 {
+				t.Errorf("got %d unresponded threads, expected 0", actionableThreads)
+			}
+		}
+
+		myComments := countCommentsByUser(threads, myUsername)
+		if myPr && !lastCommentIsMine {
+			if actionableThreads == 0 {
+				t.Error("someone is waiting on my comment but we got 0")
+			}
+		}
+
+		if !myPr && len(myComments) == 0 {
+			if actionableThreads != 0 {
+				t.Errorf("someone else's PR, and I wasn't part of the thread, should have gotten 0 but got %d", actionableThreads)
+			}
+		}
+	})
+}
+
+func countCommentsByUser(pr prSearchResultGraphQl, username string) map[int]struct{} {
+	myCommentIndexes := make(map[int]struct{})
+	for i, t := range pr.ReviewThreads.Edges {
+		if t.Node.IsCollapsed || t.Node.IsOutdated || t.Node.IsResolved {
+			continue
+		}
+
+		for _, c := range t.Node.Comments.Nodes {
+			if c.Author.Login == username {
+				myCommentIndexes[i] = struct{}{}
+			}
+		}
+	}
+
+	return myCommentIndexes
+}
